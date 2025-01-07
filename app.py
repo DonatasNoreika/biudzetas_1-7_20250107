c6d8090516767971ac1661cee0f165bc56b65e49
DonatasNoreika <donoras@gmail.com>
2025-01-07 11:42:29 +0200

    pilnai aprašytas app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ biudzetas = Biudzetas()
 while True:
     choice = int(input("1 - peržiūrėti, 2 - įvesti pajamas, 3 - įvesti išlaidas, 4 - balansas, 0 - išeiti\n"))
     match choice:
-        case 1:
-            print(biudzetas.zurnalas)
-        case 0:
-            break
+        case 1: print(biudzetas.zurnalas)
+        case 2: biudzetas.prideti_pajamas()
+        case 3: biudzetas.prideti_islaidas()
+        case 4: print("Balansas", biudzetas.gauti_balansa())
+        case 0: break
